8dda3423455470e1201e266a63df1ecdaefc59da
surajbhatt-09 <srjbhatt376@gmail.com>
2019-06-16 10:03:34 +0530

    Updates

---
+++ precache-manifest.bb6dc4f767966bc6aad2d9dba783e24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe58866a3fc53b5b0edd0f7f670ebf7",
+    "url": "/newone/index.html"
+  },
+  {
+    "revision": "691dc6a34b4e99b08a1b",
+    "url": "/newone/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/newone/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "691dc6a34b4e99b08a1b",
+    "url": "/newone/static/js/main.81fb67ea.chunk.js"
+  },
+  {
+    "revision": "391ae0d8b031acf74b82",
+    "url": "/newone/static/js/runtime~main.724d1c85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newone/static/media/logo.5d5d9eef.svg"
+  }
+]);
